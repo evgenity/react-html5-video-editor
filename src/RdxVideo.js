df2ab52fa82e0f4836f1cbd0190c1a95d824aade
Evgeni Bikov <bikovevg@gmail.com>
2016-07-10 00:37:21 +0300

    <script>-tag support added

--- src/RdxVideo.js
+++ src/RdxVideo.js
@@ -4,6 +4,7 @@ import * as actionCreators from './actions/actionCreators';
 import Video from './components/Video';
 import Controls from './components/Controls';
 import Overlay from './components/Overlay';
+import store from './store'
 
 
 function mapStateToProps(state) {
@@ -14,4 +15,19 @@ function mapStateToProps(state) {
 
 const RdxVideo = connect(mapStateToProps)(Video);
 
-export {RdxVideo, Overlay, Controls};
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function render_editor(poster="src/img/poster.png", vid_path="src/video/small.mp4") {
+	ReactDOM.render(
+		<RdxVideo autoPlay loop muted poster={poster} store={store}>
+			<Overlay />
+			<Controls />
+			<source src={vid_path} type="video/mp4" />
+		</RdxVideo>
+		,
+		document.getElementById('root')
+	);
+}
+
+export {RdxVideo, Overlay, Controls, store, render_editor};
